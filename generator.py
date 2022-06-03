331810ad3e788124bbed9b170350b57310f2f54c
Ángel Carballo <50462944+AngelJuanMa@users.noreply.github.com>
2022-06-03 04:37:01 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -13,21 +13,22 @@ str = "publicKey" + privateKey
 encoded_str = str.encode()
  
 # create sha-2 hash objects initialized with the encoded string
+# Hashing algorithms can be changed: https://docs.python.org/3/library/hashlib.html
 hash_obj_sha256 = hashlib.sha256(encoded_str).hexdigest()   # SHA256
 hash_obj_sha512 = hashlib.sha512(encoded_str).hexdigest()   # SHA512
 
-# Capitalize some words
+# Capital more or different letters
 hash_obj_sha256 = hash_obj_sha256.encode("utf-8").replace(b"a", b"A").decode('utf-8')
 hash_obj_sha512 = hash_obj_sha512.encode("utf-8").replace(b"a", b"A").decode('utf-8')
 
-# The symbols "@$" could be changed to others
+# You can change the symbols for more or different, and not only at the end of the password.
 print("\nSHA256 Hash: ", hash_obj_sha256 + "@$")
 print("\nSHA512 Hash: ", hash_obj_sha512 + "@$")
 
 # Copy to clipboard the hash with algorithm sha512
 clipboard.copy(hash_obj_sha512+ "@$")
 
-# Append Private key to a file to remember keys, opcionally
+# Remove keyword saving, if you seems it unsafe
 f = open ('clave.txt','a')
 f.write('\n' + clave)
 f.close()
